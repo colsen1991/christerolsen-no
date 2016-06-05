b13904d4c4a949528f6aa1d435f4509cb66a1e8e
Christer Olsen <colsen1991@gmail.com>
2016-06-05 14:38:49 +0200

    Amasing transforms!

--- web/src/index.js
+++ web/src/index.js
@@ -1,18 +1,34 @@
-window.onerror = () => {
-  window.location = '/error';
+function bootstrap() { // eslint-disable-line no-unused-vars
+  window.onerror = () => {
+    window.location = '/error';
+  };
 
-  return true;
-};
 
-const ssc = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-let sscIndex = 0;
+  const ssc = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+  let sscIndex = 0;
 
-window.onkeydown = ({ which }) => {
-  if (ssc[sscIndex] === which) {
-    sscIndex++;
+  window.onkeydown = event => {
+    if (ssc[sscIndex] === event.which) {
+      sscIndex++;
 
-    if (sscIndex === ssc.length && confirm('goto admin?')) // eslint-disable-line
-      window.location = '/secret';
-  } else
-    sscIndex = 0;
-};
+      if (sscIndex === ssc.length && confirm('goto admin?')) // eslint-disable-line
+        window.location = '/secret';
+    } else
+      sscIndex = 0;
+  };
+
+
+  setTimeout(() => {
+    [
+      'opacity-0',
+      'transform-translate-y-minus-100',
+      'transform-translate-x-800',
+      'transform-translate-x-minus-800'
+    ].forEach(className => {
+      const els = document.getElementsByClassName(className);
+      Array.prototype.slice.call(els).forEach(el => {
+        el.classList.remove(className);
+      });
+    });
+  });
+}
