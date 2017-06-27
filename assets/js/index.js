e3415b7fbf93e84b7d749c22a65e9e968243fe73
Christer Olsen <colsen1991@gmail.com>
2017-06-27 18:58:45 +1000

    readded missing feature (thank you) after clean up

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,4 @@
-console.log('you can use ES6 here : )')
+if (window.location.search.includes('showThanks=true')) {
+  document.querySelector('#js-contact-form').style.display = 'none'
+  document.querySelector('#thank-you').style.display = 'block'
+}
