e64dbc09d6c98dc187d099c57a4b3889f2e77564
Christer Olsen <colsen1991@gmail.com>
2017-08-25 13:44:22 +0200

    And so it begins... (rewrite nuxt)

--- assets/js/index.js
+++
@@ -1,4 +0,0 @@
-if (window.location.search.includes('showThanks=true')) {
-  document.querySelector('#js-contact-form').style.display = 'none'
-  document.querySelector('#thank-you').style.display = 'block'
-}
