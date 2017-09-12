c0a7c14e9eec9b0e2945b1f4300f897d7f29aa5f
Christer Olsen <colsen1991@gmail.com>
2017-09-12 20:55:32 +0200

    Only using certain buefy components

--- plugins/buefy.js
+++ plugins/buefy.js
@@ -1,4 +1,7 @@
 import Vue from 'vue'
 import Buefy from 'buefy'
 
-Vue.use(Buefy, { defaultIconPack: 'fa' })
+// TODO: Fix hack. Unexpected '>* in buefy/src/utils/config when following https://github.com/rafaelpimpa/buefy/issues/73
+Buefy.Icon.props.pack = { type: String, default: 'fa' }
+
+Vue.component(Buefy.Icon.name, Buefy.Icon)
